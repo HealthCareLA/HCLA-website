13ea54b7887f3dc2a42d496051cf6c636ef51253
Evan Hantverk <seamanlaptop123@gmail.com>
2024-08-27 01:25:27 -0400

    update emergency care part

--- src/components/urgency_emergency/urgentcare/urgentcare.jsx
+++ src/components/urgency_emergency/urgentcare/urgentcare.jsx
@@ -13,7 +13,7 @@ export default function UrgencyUrgentCare({
   icondescriptions
 }) {
   return (
-    <>
+    <section suppressHydrationWarning>
       <div className="bg-white relative py-[50px] md:py-[100px] lg:pt-[40px] lg:pb-[200px]">
         <img
           src="/images/quick-links-shape.png"
@@ -21,7 +21,7 @@ export default function UrgencyUrgentCare({
           className="absolute left-1/2 -translate-x-1/2 w-full quick-links-shape hidden lg:block"
         />
       </div>
-      <section className={isMatch ? "bg-sky-blue text-blue py-[50px] md:py-[80px] lg:pb-[100px]" : "bg-blue text-white py-[50px] md:py-[80px] lg:pt-[120px] lg:pb-[25px]"} suppressHydrationWarning>
+      <div className={isMatch ? "bg-sky-blue text-blue py-[50px] md:py-[80px] lg:pb-[100px]" : "bg-blue text-white py-[50px] md:py-[80px] lg:pt-[120px] lg:pb-[25px]"}>
         <div className="max-container flex flex-col justify-between gap-x-5 gap-y-10 relative lg:pt-[50px]">
 
           <div className="flex flex-col-reverse lg:flex-row items-start justify-between gap-[50px]">
@@ -84,7 +84,7 @@ export default function UrgencyUrgentCare({
           </div>
 
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
