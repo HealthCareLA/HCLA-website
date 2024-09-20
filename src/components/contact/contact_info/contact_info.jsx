cf90d9ec2e35e06e704a0d93f6f20c92ce2d9b01
Evan Hantverk <seamanlaptop123@gmail.com>
2024-09-20 09:25:26 -0400

    add contact page

---
+++ src/components/contact/contact_info/contact_info.jsx
@@ -0,0 +1,45 @@
+import MarkdownIt from "markdown-it";
+const md = new MarkdownIt({ html: true });
+const isMatch = true;
+
+export default function ContactInfo(block) {
+  return (
+    <section className="bg-white pt-[50px] lg:pt-[50px] xl:pt-[85px] xxl:pt-[85px] pb-[50px] lg:pb-[80px] xl:pb-[85px]" suppressHydrationWarning id="Grants_Collaboration">
+      <div className="max-container">
+        <h2 className="text-blue font-medium">{block.title}</h2>
+        <div class="flex flex-wrap">
+          <div className="w-full lg:w-1/2 mb-[30px] lg:mb-0">
+            <div className="w-full flex justify-center">
+              <img src={block.icon1} />
+            </div>
+            {
+              block.subtitle1 && (
+                <h4 className="text-[28px] lg:max-w-[400px] my-[15px] text-common-clr">{block.subtitle1}</h4>
+              )
+            }
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr" dangerouslySetInnerHTML={{ __html: block.description1 }}>
+            </p>
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr mt-[30px]" dangerouslySetInnerHTML={{ __html: block.phone_desc }}>
+            </p>
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr" dangerouslySetInnerHTML={{ __html: block.phone_details }}>
+            </p>
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr mt-[30px]" dangerouslySetInnerHTML={{ __html: block.operation_hours }}>
+            </p>
+          </div>
+          <div className="w-full lg:w-1/2  flex flex-col gap-[22px]">
+            <div className="w-full flex justify-center">
+              <img src={block.icon2} />
+            </div>
+            {
+              block.subtitle2 && (
+                <h4 className="text-[28px] lg:max-w-[400px] my-[15px] text-common-clr">{block.subtitle2}</h4>
+              )
+            }
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr" dangerouslySetInnerHTML={{ __html: block.part2_description1 }}></p>
+            <p className="max-w-full lg:max-w-[480px] p2 whitespace-pre-line text-common-clr" dangerouslySetInnerHTML={{ __html: block.part2_description2 }}></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/content/pages/contact.md
@@ -0,0 +1,42 @@
+---
+_schema: default
+title: Contact US
+n_type: "2"
+seo:
+  page_description:
+  canonical_url:
+  featured_image:
+  featured_image_alt:
+  author_twitter_handle:
+  open_graph_type:
+  no_index: false
+content_blocks:
+  - _bookshop_name: contact/hero
+    title: Contact US
+    image:
+      src: /images/ways-we-serve.png
+      alt: banner-image
+      width: 550
+      height: 550
+      isRounded: true
+  - _bookshop_name: contact/contact_info
+    title: Contact Information
+    subtitle1: General Questions & Support
+    icon1: /images/icon-advocacy.svg
+    description1: >-
+      <strong>Current physicians or patients,</strong> please contact our management company,
+      MedPOINT Management at:
+    phone_desc: >-
+      <strong>Main Phone</strong>: 818-702-0100
+    phone_details: (TTY/TDD users should dial 711.)
+    operation_hours: >-
+      <strong>Hours of Operation</strong>: M – F 8:00 a.m. – 5:00 p.m.
+
+    subtitle2: Grant Liaison & General Inquiries
+    icon2: /images/icon-build.svg
+    part2_description1: >-
+      <strong>For grant program questions</strong>, please contact Health Care LA at (818) 702-0100 and ask to be transferred to our grant liaison.
+    part2_description2: >-
+      <strong>For general inquiries,</strong> please email <strong class="text-blue">info@healthcarela.org</strong>.
+
+---
